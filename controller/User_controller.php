ec6f005dcd1b9a4b0d3502b16e1f5ed16d92a082
thomas-doan <thomas.doan@laplateforme.io>
2021-12-23 10:09:02 +0100

    initialisation du projet : Architecture MVC poo, step 1 mysqli

---
+++ controller/User_controller.php
@@ -0,0 +1,3 @@
+<?php
+require_once(__DIR__ . "/../model/Connection_model.php");
+require_once(__DIR__ . "/../model/User_model.php");
---
+++ index.php
@@ -0,0 +1,32 @@
+<?php
+session_start();
+require_once("./controllers/User_controller.php");
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Projet poo</title>
+    <link rel="stylesheet" href="./public/css/style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" integrity="sha512-Fo3rlrZj/k7ujTnHg4CGR2D7kSs0v4LLanw2qksYuRlEzO+tcaEPQogQ0KaoGN26/zrn20ImR1DfuLWnOo7aBA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+    <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+</head>
+
+<body>
+    <main>
+
+    </main>
+    <script>
+        AOS.init({
+            duration: 2000,
+        })
+    </script>
+</body>
+
+</html>
